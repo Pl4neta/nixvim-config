bf3f5171982897e7a1d5c8266ed2a08d882631e6
Pl4neta <135662804+Pl4neta@users.noreply.github.com>
2024-11-01 17:02:12 +0000

    harpoon

--- config/plugins.nix
+++ config/plugins.nix
@@ -463,6 +463,19 @@
       };
     };
 
+    harpoon = {
+      enable = true;
+      keymaps = {
+	addFile = "<leader>ha";
+	toggleQuickMenu = "<C-e>";
+	navFile = {
+	  "1" = "<leader>hj";
+	  "2" = "<leader>hk";
+	  "3" = "<leader>hl";
+	  "4" = "<leader>hm";
+	};
+      };
+    };
   };
 
   extraConfigVim = "";
